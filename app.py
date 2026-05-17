d1272d67bf7912184abd43eb26d5571bd9ff8cbc
nguyenjahuy56-hub <nguyenjahuy56@gmail.com>
2026-05-17 13:59:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,15 +10,15 @@ from pymongo import MongoClient
 # ================= CẤU HÌNH THÔNG SỐ AI (ĐÃ TÁCH RIÊNG & THÊM TRỌNG SỐ THỜI GIAN) =================
 
 # ♠️ THÔNG SỐ AI CHO SUNWIN
-SUNWIN_MAX_SAMPLES_TONG = 20     # Số lần quét lại lịch sử cho TỔNG 
-SUNWIN_MAX_SAMPLES_DICE = 30    # Số lần quét lại lịch sử cho BỘ XÚC XẮC 
+SUNWIN_MAX_SAMPLES_TONG = 4     # Số lần quét lại lịch sử cho TỔNG 
+SUNWIN_MAX_SAMPLES_DICE = 2    # Số lần quét lại lịch sử cho BỘ XÚC XẮC 
 SUNWIN_WEIGHT_TONG = 0.50       # Trọng số của Tổng so với Xúc Xắc (45%)
 SUNWIN_WEIGHT_DICE = 0.50       # Trọng số của Xúc Xắc so với Tổng (55%)
-SUNWIN_SAMPLE_DECAY = 0.85      # Độ suy giảm trọng số mẫu cũ (1.0 là ko giảm. 0.85 là tối ưu nhất: 100% -> 85% -> 72%...)
+SUNWIN_SAMPLE_DECAY = 1      # Độ suy giảm trọng số mẫu cũ (1.0 là ko giảm. 0.85 là tối ưu nhất: 100% -> 85% -> 72%...)
 
 # ♦️ THÔNG SỐ AI CHO HITCLUB MD5
-HITCLUB_MAX_SAMPLES_TONG = 13    
-HITCLUB_MAX_SAMPLES_DICE = 8    
+HITCLUB_MAX_SAMPLES_TONG = 7    
+HITCLUB_MAX_SAMPLES_DICE = 5    
 HITCLUB_WEIGHT_TONG = 0.50      
 HITCLUB_WEIGHT_DICE = 0.50      
 HITCLUB_SAMPLE_DECAY = 0.90    
