83ff9484d42b8aeb8db65ecf4adb6286bd5ff3ea
nguyenjahuy56-hub <nguyenjahuy56@gmail.com>
2026-05-17 08:29:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import hashlib
 from pymongo import MongoClient
 
 # ================= CẤU HÌNH THÔNG SỐ AI =================
-MAX_SAMPLES_TONG = 8  
-MAX_SAMPLES_DICE = 13  
+MAX_SAMPLES_TONG = 5  
+MAX_SAMPLES_DICE = 4  
 WEIGHT_TONG = 0.45     
 WEIGHT_DICE = 0.55     
 # ========================================================
